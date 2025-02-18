49278863c23ae9a03c05d5e02e5218d897e0795d
Oleksandra Hulchuk <dd221285gaa@gmail.com>
2025-02-17 20:23:32 -0600

    fetching data from database added

--- src/app/blog/posts/page.tsx
+++ src/app/blog/posts/page.tsx
@@ -1,10 +1,14 @@
-import { posts } from '@/app/lib/placeholder-data';
+// import { posts } from '@/app/lib/placeholder-data';
 import Post from '@/app/ui/components/posts/Post';
+import {getPosts} from '@/app/lib/data';
 
-export default function Page() {
-  return (
-    <>
-      <h1>Posts</h1>
-      {posts.map((post) => <Post key={post.id} {...post} />)}
-    </>)
+export default async function Page() {
+
+    const posts = await getPosts();
+    return (
+        <>
+            {/*{client && <p className='text-green-500'>Connected to database</p>}*/}
+            <h1>Posts</h1>
+            {posts?.map((post) => <Post id={''} title={''} content={''} date={''} key={post.id} {...post} />)}
+        </>)
 }
---
+++ src/app/lib/data.ts
@@ -0,0 +1,30 @@
+import {createClient} from "@vercel/postgres";
+
+export async function connectToDB() {
+    const client = createClient();
+    await client.connect();
+
+    try {
+        if (client) {
+            console.log('Connected to database');
+            return client;
+        }
+    } catch (err) {
+        console.error('Error connecting to database', err);
+    }
+}
+
+export async function getPosts() {
+    try {
+        const client = await connectToDB();
+        if (!client) {
+            throw new Error('Database client is undefined');
+        }
+        const data = await client.sql`SELECT *
+                                      FROM posts LIMIT 5;`;
+        // console.log(data.rows);
+        return data.rows;
+    } catch (err) {
+        console.error('Error getting posts', err);
+    }
+}
