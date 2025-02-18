7ddd28d3ed5cc88d5820f965771a916c28086055
Oleksandra Hulchuk <dd221285gaa@gmail.com>
2025-02-17 22:52:40 -0600

    error handling added; links to post fixed

--- src/app/blog/post/[id]/page.tsx
+++ src/app/blog/post/[id]/page.tsx
@@ -1,11 +1,17 @@
-import { posts } from '@/app/lib/placeholder-data';
+// import { posts } from '@/app/lib/placeholder-data';
+import { getPosts } from '@/app/lib/data';
 import Post from '@/app/ui/components/posts/Post';
+import { notFound } from "next/navigation";
 
-export default function Page({ params }: { params: { id: string } }) {
-  const post = posts.find((post) => post.id === params.id) ?? { id: '', title: '', content: '', date: '' };
+export default async function Page({ params }: { params: { id: string } }) {
+    const posts = await getPosts();
+    const post = posts?.find((post) => post.id === params.id);
+  if (!post) {
+      notFound();
+  }
   return (
     <>
       <h1>Post</h1>
-      <Post {...post} />
+        {post && <Post id={''} title={''} content={''} date={''} {...post} />}
     </>)
 }
